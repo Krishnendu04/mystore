70e6a24d92b28a3ab415b4da20b1678bef254149
krishnendu04 <krishnendupurkait0@gmail.com>
2026-01-22 12:08:53 +0530

    Fix file path casing and import spelling

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/Home";
+import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import ProductDetails from "../Pages/ProductDetails";
 import About from "../Pages/About";
